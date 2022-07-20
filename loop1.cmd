90627b7887c7d95954009f25e1329a7187862b7c
crceck123 <100026828+crceck123@users.noreply.github.com>
2022-07-20 20:18:40 +0400

    Update loop1.cmd

--- loop1.cmd
+++ loop1.cmd
@@ -1,6 +1,780 @@
-:1
 ping 1.1.1.1
 ping 8.8.8.8
 echo OK
 cls
-goto 1
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
+ping 1.1.1.1
+ping 8.8.8.8
+echo OK
+cls
+
